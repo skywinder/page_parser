b4a60d4d0bb8c109b6bbcc5f3318f7d811092069
Petr Korolev <sky4winder@gmail.com>
2015-11-23 16:43:27 +0200

    parse brands

---
+++ ejuices_parser.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'nokogiri'
+require 'restclient'
+
+BASE_URL = 'http://www.ejuices.co'
+WEB_STRING = "http://www.ejuices.co/collections/all-brands"
+
+
+def get_all_brands_links(page)
+  dropdowns = page.css('ul.dropdown-wrap')
+  brands= dropdowns[1]
+  brands_links =brands.css('li.dropdown-item').map { |x| x.css('a') }
+  # brands_links.each {
+  #     |li| puts li[0]['href']
+  # }
+  brands_links
+  # puts page.search('li.dd-li')
+  # puts brands_links.count
+
+end
+
+def parse
+  page = Nokogiri::HTML(RestClient.get(WEB_STRING))
+  brands_links = get_all_brands_links(page)
+  all_brands_links = brands_links.map { |x| BASE_URL+x[0]['href'] }
+  # puts all_brands_links
+  all_brands_names = brands_links.map { |x| x[0].text }
+
+  all_brands_names.zip(all_brands_links).take(5) { |n, l| puts "#{n} #{l}" }
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  parse # => Nokogiri::HTML::Document
+end
--- parser.rb
+++ parser.rb
@@ -4,12 +4,22 @@ require 'restclient'
 
 # WEB_STRING = "http://www.vape-room-cyprus.com/"
 # WEB_STRING = "http://www.ejuices.co/collections/all-brands"
-WEB_STRING = "http://www.ejuices.co/collections/all-brands"
+WEB_STRING = "http://ruby.bastardsbook.com/files/hello-webpage.html"
 
 
 def parse
   page = Nokogiri::HTML(RestClient.get(WEB_STRING))
   puts page.class # => Nokogiri::HTML::Document  puts page.class
+  xml = page.css('li')[2]
+  links = page.css("a")
+
+  l = links.map { |x| x["href"] }
+
+  news_links = page.css("div#references a")
+  news_links.each{|link| puts "#{link.text}\t#{link['href']}"}
+  puts
+
+
 end
 
 
