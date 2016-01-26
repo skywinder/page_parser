5cac89ce1ffacf29dd17c0b244bcaf7b54e85281
Petr Korolev <sky4winder@gmail.com>
2016-01-26 15:58:50 +0200

    cm

--- liquid_option.rb
+++ liquid_option.rb
@@ -1,14 +1,15 @@
 module PageParser
   class LiquidOption
+    attr_reader :image_url
     attr_accessor :bottle_size
     attr_accessor :nic_level
-    attr_accessor :wholesale_price, :image_url
+    attr_accessor :wholesale_price
 
     def initialize(option, img)
       o_gsub = option.gsub(/\s+/, "")
       o_split = o_gsub.split(/[,\/-]/)
       @bottle_size, @nic_level, @wholesale_price = o_split
-      @image_url = "https:#{img}"
+      @image_url = "#{img}"
     end
 
     MULTIPLIER = 2.0
@@ -18,9 +19,14 @@ module PageParser
       price_double = price.to_f * MULTIPLIER
       price_double
     end
+
     def bottle_size_num
       bottle_size_gsub = @bottle_size.gsub(/\D/, '')
       bottle_size_gsub
+    end
+
+    def image_url
+      "https:#{@image_url}"
     end
   end
 end
